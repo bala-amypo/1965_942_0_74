11a21eeef60e8bc3891340f2d2cbc8b8b26dceb3
bala-amypo <bala@amypo.in>
2025-12-17 09:01:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/stuentity.java
+++ src/main/java/com/example/demo/entity/stuentity.java
@@ -1,51 +1,54 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistance.GeneratedValue;
-import jakarta.persistance.GenerationType;
-import jakarta.persistance.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
-public class Stuentity{
+public class stuentity{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private float cgpa;
 
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
 
-public long getId(){
-    return id;
- }
- public void setId(Long id){
-    this.id = id;
- }
- public String getName(){
-    return name;
- }
- public void setName(String name){
-    this.name = name;
- }
- public String getEmail(){
-    return email;
- }
- public void setEmail(String email){
-    this.email=email;
- }
- public float getEmail(){
-    return cgpa;
- }
- public void setcgpa(String cgpa){
-    this.cgpa=cgpa;
- }
- public Stuentity(Long id,String name, String email,float cgpa){
-    this.id=id;
-    this.name=name;
-    this.email=email;
-    this.cgpa=cgpa;
- }
- public Stuentity(){
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 
- }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
 
+    public float getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(float cgpa){
+        this.cgpa=cgpa;
+    }
+    public stuentity(Long id,String name,String email,float cgpa){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.cgpa=cgpa;
+
+    }
+    public stuentity(){
+        
+    }
 }
